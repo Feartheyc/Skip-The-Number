c47ad2e2ed18e5f9415d7e5654338016cf9b316b
Bhavyapatel2807 <bhavya2807@gmail.com>
2026-03-23 14:38:32 +0530

    deletions made

--- main.js
+++ main.js
@@ -45,12 +45,8 @@ window.startGame = function (gameName) {
   document.getElementById("input_video").style.opacity = "1";
 
   if (gameName === "game1") { window.currentGame = Game1; Game1.init(); }
-  if (gameName === "game2") { window.currentGame = Game2; Game2.init(); }
   if (gameName === "Game3") { window.currentGame = Game3; Game3.init(); Game3.startDetection(); }
-  if (gameName === "Game4") { window.currentGame = Game4; Game4.init(); }
   if (gameName === "Game5") { window.currentGame = Game5; Game5.init(); }
-  if (gameName === "Game6") { window.currentGame = Game6; Game6.init(); }
-  if (gameName === "Game7") { window.currentGame = Game7; Game7.init(); }
   if (gameName === "Game8") { window.currentGame = Game8; Game8.init(); }
   if (gameName === "Game9") { window.currentGame = Game9; Game9.init(); }
   if (gameName === "Game10") {
